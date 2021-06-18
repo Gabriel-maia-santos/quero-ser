b2e2d481fb83c86b69e90e9a51e8e5bb8ce61eae
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2021-06-18 19:02:18 -0300

    Feito

---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota_100 = 0;
+            int nota_50 = 0;
+            int nota_20 = 0;
+            int nota_10 = 0;
+
+            Console.Write("Quantidade: ");
+            int number = Int32.Parse(Console.ReadLine());
+
+            if(number >= 100) {
+                nota_100 = number / 100;
+                number = number - 100 * nota_100;
+            if(number >= 50){
+                nota_50 = (number / 50);
+                number = number - 50 * nota_50;
+            if(number >= 20){
+                nota_20 = number / 20;
+                number = number - 20 * nota_20;
+            if(10 <= number){
+                nota_10 = number / 10;
+                number = number - 10 * nota_10;
+                    }
+                }
+            }
+        }
+            
+            Console.Write("Resultado Esperado: ");
+
+            if(nota_100 != 0){
+            Console.Write($"Entregar {nota_100} notas de R$100,00");
+            }
+            if(nota_50 != 0 ){
+                Console.Write($" , {nota_50} notas de R$50,00");
+            }
+            if(nota_20 != 0){
+                Console.Write($" , {nota_20} notas de R$20,00");
+            }
+            if(nota_10 !=0){
+                Console.Write($" e {nota_10} notas de R$10,00");
+            }
+        }
+    }
+}
---
+++ Jokenpo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Jokenpo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro Jogador: ");
+            string Jogar = Console.ReadLine();
+            
+            Console.WriteLine("Digite o segundo Jogador: ");
+            string Jogar2 = Console.ReadLine();
+
+            string Jogador1 = "Jogador 1 Ganhou";
+            string Jogador2 = "Jogador 2 Ganhou";
+
+            string Empate = "Empate";
+
+            string Pedra = "Pedra";
+            string Tesoura = "Tesoura";
+            string Papel = "Papel";
+
+            Console.WriteLine("Resultado:");
+            
+            if(Jogar == Tesoura && Jogar2 == Pedra ){
+                Console.WriteLine($"{Jogador2}");
+            }
+            if(Jogar == Tesoura && Jogar2 == Papel ){
+                Console.WriteLine($"{Jogador1}");
+            }
+            if(Jogar == Pedra && Jogar2 == Tesoura){
+                Console.WriteLine($"{Jogador1}");
+            }
+            if(Jogar == Pedra && Jogar2 == Papel ){
+                Console.WriteLine($"{Jogador2}");
+            }
+            if(Jogar == Papel && Jogar2 == Pedra ){
+                Console.WriteLine($"{Jogador1}");
+            }
+            if(Jogar == Papel && Jogar2 == Tesoura ){
+                Console.WriteLine($"{Jogador2}");
+            }
+            if(Jogar == Papel && Jogar2 == Papel ){
+                Console.WriteLine($"{Empate}");
+            }
+            if(Jogar == Tesoura && Jogar2 == Tesoura ){
+                Console.WriteLine($"{Empate}");
+            }
+            if(Jogar == Pedra && Jogar2 == Pedra ){
+                Console.WriteLine($"{Empate}");
+            }
+            
+        }
+    }
+}
